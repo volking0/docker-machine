d77d3e3049a76450d6c59d679ba1db5ecc2fdfb3
volking0 <volking0@gmail.com>
2021-05-20 15:24:23 +0300

    generate certs manually

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir certs
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout certs/dockerrepo.key -x509 -days 365 -out certs/dockerrepo.crt -subj /CN=local-registry
 docker-machine create --driver=none --url=tcp://${DOCKER_MACHINE_URL}:${DOCKER_MACHINE_PORT} ${DOCKER_MACHINE_NAME}
 
 eval "$(docker-machine env ${DOCKER_MACHINE_NAME} --shell ash)"
